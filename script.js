3fd2dd4370181a0811fb79c121ca3e09a6404bc1
Martina Kurková <martinanoha@seznam.cz>
2024-09-30 20:07:33 +0200

    "kostka_hotovo"

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ const predstaveni = 15;
 let prijemDivadla = vstupne * divaci * predstaveni;
 document.body.innerHTML += "<p>" + "Příjem divadla je " + prijemDivadla + "Kč" + "</p>";
 
-const sleva = 0.65;
+const sleva = 0.35;
 const studenti = 0.4;
 
-let prijemDivadlaStudenti = Math.floor((prijemDivadla * 0.4) * 0.35);
+let prijemDivadlaStudenti = Math.floor((prijemDivadla * studenti) * sleva);
 let novyPrijemCelkem = prijemDivadla - prijemDivadlaStudenti;
 
 document.body.innerHTML += "<p>" + "Příjem divadla se studentským vstupným je " + novyPrijemCelkem + "Kč" + "</p>";
+
+//hod kostkou
+
+let kostka;
+
+kostka = Math.floor(Math.random() * 6) + 1;
+
+document.body.innerHTML += "<p>" + kostka + "</p>";
