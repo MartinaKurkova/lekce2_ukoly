ade779820715788522eb52c64030c113e2eafbdb
Martina Kurková <martinanoha@seznam.cz>
2024-09-30 20:01:03 +0200

    "divadlo dokonceni"

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-// divadlo
+// příjem divadla
 const vstupne = 12;
-const navstevnici = 174;
+const divaci = 174;
 const predstaveni = 15;
+
+let prijemDivadla = vstupne * divaci * predstaveni;
+document.body.innerHTML += "<p>" + "Příjem divadla je " + prijemDivadla + "Kč" + "</p>";
+
+const sleva = 0.65;
+const studenti = 0.4;
+
+let prijemDivadlaStudenti = Math.floor((prijemDivadla * 0.4) * 0.35);
+let novyPrijemCelkem = prijemDivadla - prijemDivadlaStudenti;
+
+document.body.innerHTML += "<p>" + "Příjem divadla se studentským vstupným je " + novyPrijemCelkem + "Kč" + "</p>";
